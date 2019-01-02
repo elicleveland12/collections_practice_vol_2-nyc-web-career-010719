259106bd080aec3972aa9d49053b9950e3554f8f
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 20:18:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,7 +47,9 @@ def find_cool(cool)
 end
 
 def organize_schools(schools)
-  
+    locations_hash = {}
+    schools.collect {|k,v| locations_hash[v[:location]] = []}
+    locations_hash.each {|k,v| schools.each {|k1,v1| if k == v1[:location] then v << k1  end}}
 end
 
   
